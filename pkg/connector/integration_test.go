5acc21611eaa7a5e59e9bcf8c0b49da6ad571872
Javier David Carnelli <javier.carnelli@conductorone.com>
2025-02-07 02:23:04 -0300

    test: test for the resource builders added

---
+++ pkg/connector/integration_test.go
@@ -0,0 +1,81 @@
+package connector
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/conductorone/baton-freshdesk/pkg/client"
+	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
+	"github.com/conductorone/baton-sdk/pkg/pagination"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	ctx              = context.Background()
+	domain, _        = os.LookupEnv("FRESHDESK_DOMAIN")
+	apikey, _        = os.LookupEnv("FRESHDESK_TOKEN")
+	parentResourceID = &v2.ResourceId{}
+	pToken           = &pagination.Token{Size: 50, Token: ""}
+)
+
+func TestUserBuilderList(t *testing.T) {
+	if apikey == "" || domain == "" {
+		message := fmt.Sprintf("params not found. apikey: %s - domain: %s", apikey, domain)
+		t.Skip(message)
+	}
+	fc := client.NewClient().WithBearerToken(apikey).WithDomain(domain)
+	c, _ := client.New(ctx, fc)
+	u := newUserBuilder(c)
+
+	res, _, _, err := u.List(ctx, parentResourceID, pToken)
+	assert.Nil(t, err)
+	assert.NotNil(t, res)
+
+	message := fmt.Sprintf("Amount of users obtained: %d", len(res))
+	t.Log(message)
+}
+
+func TestRoleBuilderList(t *testing.T) {
+	if apikey == "" || domain == "" {
+		message := fmt.Sprintf("params not founc. apikey: %s - domain: %s", apikey, domain)
+		t.Skip(message)
+	}
+
+	fc := client.NewClient().WithBearerToken(apikey).WithDomain(domain)
+	c, err := client.New(ctx, fc)
+	if err != nil {
+		t.Errorf("ERROR: Failed to create client: %v", err)
+	}
+
+	r := newRoleBuilder(c)
+
+	res, _, _, err := r.List(ctx, parentResourceID, pToken)
+	assert.Nil(t, err)
+	assert.NotNil(t, res)
+
+	message := fmt.Sprintf("Amount of roles obtained: %d", len(res))
+	t.Log(message)
+}
+
+func TestGroupBuilderList(t *testing.T) {
+	if apikey == "" || domain == "" {
+		message := fmt.Sprintf("params not founc. apikey: %s - domain: %s", apikey, domain)
+		t.Skip(message)
+	}
+
+	fc := client.NewClient().WithBearerToken(apikey).WithDomain(domain)
+	c, err := client.New(ctx, fc)
+	if err != nil {
+		t.Errorf("ERROR: Failed to create client: %v", err)
+	}
+
+	g := newGroupBuilder(c)
+	res, _, _, err := g.List(ctx, parentResourceID, pToken)
+	assert.Nil(t, err)
+	assert.NotNil(t, res)
+
+	message := fmt.Sprintf("Amount of groups obtained: %d", len(res))
+	t.Log(message)
+}
