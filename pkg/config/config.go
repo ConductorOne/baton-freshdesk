831b7870574ce7d2e69b042a03f57cb1e7573e0e
Felipe Lucero <felijlucero@gmail.com>
2025-09-11 16:44:30 -0300

    Update baton sdk and containerizing

---
+++ pkg/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"github.com/conductorone/baton-sdk/pkg/field"
+)
+
+const (
+	apiKey = "api-key"
+	domain = "domain"
+)
+
+var (
+	apiKeyField = field.StringField(
+		apiKey,
+		field.WithDisplayName("API Key"),
+		field.WithIsSecret(true),
+		field.WithRequired(true),
+		field.WithDescription("Freshdesk account API key"),
+	)
+	domainField = field.StringField(
+		domain,
+		field.WithDisplayName("Domain"),
+		field.WithRequired(true),
+		field.WithDescription("Freshdesk account domain"),
+	)
+
+	ConfigurationFields = []field.SchemaField{apiKeyField, domainField}
+
+	FieldRelationships = []field.SchemaFieldRelationship{}
+
+	Configuration = field.NewConfiguration(
+		ConfigurationFields,
+		field.WithConstraints(FieldRelationships...),
+		field.WithConnectorDisplayName("Freshdesk"),
+		field.WithHelpUrl("/docs/baton/freshdesk"),
+		field.WithIconUrl("/static/app-icons/freshdesk.svg"),
+	)
+)
